9b108467e36a2cd7a82de772d8024dfff2a978f4
Davin <davin.adisuryo@gmail.com>
2026-05-20 22:20:18 +0700

    Implement caching system with thread-safe operations for storing cached responses

--- caching-proxy/internal/cache/cache.go
+++ caching-proxy/internal/cache/cache.go
@@ -1,1 +1,46 @@
 package cache
+
+import (
+	"sync"
+)
+
+type CachedResponse struct {
+	StatusCode int
+	Headers    map[string]string
+	Body       []byte
+}
+
+func NewCacheSystem() *CacheSystem{
+	return &CacheSystem{
+		storage: make(map[string]CachedResponse),
+	}
+}
+type CacheSystem struct {
+	lock    sync.Mutex
+	storage map[string]CachedResponse
+}
+
+func (c *CacheSystem) Get(key string) (CachedResponse, bool) {
+	c.lock.Lock()
+
+	defer c.lock.Unlock()
+
+	item, exists := c.storage[key]
+	if !exists {
+		return CachedResponse{}, false
+	}
+	return item, true
+}
+
+func (c *CacheSystem) Set(key string, response CachedResponse) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	c.storage[key] = response
+}
+
+func (c *CacheSystem) Clear() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.storage = make(map[string]CachedResponse)
+}
