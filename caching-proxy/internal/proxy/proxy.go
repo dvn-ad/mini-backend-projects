f2b71313ae0718dda364b1493391cf0e9bde00f3
Davin <davin.adisuryo@gmail.com>
2026-05-21 08:27:02 +0700

    Improve error handling and cache invalidation in proxy server

--- caching-proxy/internal/proxy/proxy.go
+++ caching-proxy/internal/proxy/proxy.go
@@ -2,6 +2,7 @@ package proxy
 
 import (
 	"caching-proxy/internal/cache"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httputil"
@@ -33,12 +34,19 @@ func (p *ProxyServer) ProxyHandler(w http.ResponseWriter, r *http.Request){
 		p.handleCacheMiss(w,r,cacheKey)
 		return
 	}
+
 	p.handleDirectForward(w, r)
 }
 
 func (p *ProxyServer) handleCacheMiss(w http.ResponseWriter, r *http.Request, key string){
 	targetURL:=p.OriginURL+r.URL.Path
-	resp,_:=http.Get(targetURL)
+	resp,err:=http.Get(targetURL)
+	if err!=nil{
+		fmt.Println("Network error: ",err)
+		w.WriteHeader(http.StatusBadGateway)
+		w.Write([]byte("Bad Gateway"))
+		return
+	}
 	defer resp.Body.Close()
 	
 	body,_:=io.ReadAll(resp.Body)
@@ -53,8 +61,46 @@ func (p *ProxyServer) handleCacheMiss(w http.ResponseWriter, r *http.Request, ke
 	w.Write(body)
 }
 
-func (p *ProxyServer) handleDirectForward(w http.ResponseWriter, r *http.Request){
-	structuredURL, _:=url.Parse(p.OriginURL)
-	proxy:=httputil.NewSingleHostReverseProxy(structuredURL)
-	proxy.ServeHTTP(w,r)
+// func (p *ProxyServer) handleDirectForward(w http.ResponseWriter, r *http.Request){
+// 	structuredURL, _:=url.Parse(p.OriginURL)
+// 	proxy:=httputil.NewSingleHostReverseProxy(structuredURL)
+
+// 	proxy.ModifyResponse=func(resp *http.Response)error{
+
+// 		if (resp.StatusCode==200 || resp.StatusCode==201) && 
+// 		(resp.Request.Method=="PUT" || resp.Request.Method=="POST" || 
+// 		resp.Request.Method=="DELETE"){
+// 			cache.NewCacheSystem().Clear()	
+// 			fmt.Println("Mutation deteccted, cache invalidated")
+// 		}
+
+// 		proxy.ServeHTTP(w,r)
+// 		return nil
+// 	}
+
+// }
+
+func (p *ProxyServer) handleDirectForward(w http.ResponseWriter, r *http.Request) {
+	structuredURL, err := url.Parse(p.OriginURL)
+	if err != nil {
+		fmt.Println("Invalid origin URL configured:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(structuredURL)
+
+	proxy.ModifyResponse = func(resp *http.Response) error {
+		isSuccess := resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusCreated
+		isMutation := r.Method == "POST" || r.Method == "PUT" || r.Method == "DELETE"
+
+		if isSuccess && isMutation {
+			p.Cache.Clear()
+			fmt.Println("Mutation detected, active cache invalidated safely.")
+		}
+
+		return nil
+	}
+
+	proxy.ServeHTTP(w, r)
 }
